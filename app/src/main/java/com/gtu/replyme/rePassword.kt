e5fa46a515d87ef9cfec69bb5c6cc49fb3207134
buselbirgic <72623006+buselbirgic@users.noreply.github.com>
2020-12-02 01:04:32 +0300

    send email

--- app/src/main/java/com/gtu/replyme/rePassword.kt
+++ app/src/main/java/com/gtu/replyme/rePassword.kt
@@ -1,11 +1,29 @@
 package com.gtu.replyme
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_re_password.*
+import com.google.firebase.auth.FirebaseAuth
 
 class rePassword : AppCompatActivity() {
+
+    private lateinit var auth : FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_re_password)
+        supportActionBar?.hide()
+        auth = FirebaseAuth.getInstance()
+    }
+
+    fun sendEmail(view: View) {
+        val email = Email_Send.text.toString()
+        auth.sendPasswordResetEmail(email).addOnCompleteListener { task ->
+            if(task.isSuccessful) {
+                Toast.makeText( this,"Email Sent", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
